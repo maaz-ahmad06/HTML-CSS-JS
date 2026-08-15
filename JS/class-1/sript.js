5f3c430408157c3fe8050ab7e96f89d36bf96f75
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-15 21:29:57 +0500

    Class 1 | History & Introduction of JS

---
+++ JS/class-1/sript.js
@@ -0,0 +1,10 @@
+console.log("hello world!");
+
+// alert("Hello worlld!")
+
+// confirm("whats up?")
+
+let Prompt = prompt("Enter something");
+// document.write(Prompt);
+
+console.log(Prompt >=18 ? "you are adult" : "you are boy");
