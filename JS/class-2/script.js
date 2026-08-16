c5913a8026e74657783f9ac2a83e432c742f0817
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-16 11:00:21 +0500

    Class 2 | Variables in js

---
+++ JS/class-2/script.js
@@ -0,0 +1,19 @@
+var name = "test";
+console.log(name);
+
+var name = "test123";
+console.log(name);
+
+let user_name = "asdf";
+console.log(user_name);
+
+// Redeclaretion is not allowed you can reasign
+// let user_name = "asgsgas";
+user_name = "testing";
+
+const constant_name = "ali";
+console.log(constant_name);
+
+// you can not redeclare and reasign a variable when you use const key word
+// const constant_name = "ahmad";
+// constant_name = "akbar";
