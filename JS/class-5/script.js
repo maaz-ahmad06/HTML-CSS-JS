eb4ceb790521b0480e7d3986ae76b68591826fcb
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-29 10:32:51 +0500

    Class 5 | if else | ternary | else if Conditions

---
+++ JS/class-5/script.js
@@ -0,0 +1,43 @@
+/* 
+Conditional Statements:
+if
+else
+else if
+
+Ternary Operator:
+condiition ? "fulfiled" : "failed"
+
+switch (){
+    case 1:
+}
+*/
+let age = Number(prompt("Enter your age: ", 18));
+let gender = prompt("Enter your gender: ", "male/female")
+
+/* 
+syntex:
+if(condition){
+    body
+}
+*/
+
+// if(age >= 18){
+//     document.write("You are Aligible");
+// }else{
+//     document.write("You are not aligible");
+// }
+
+// alternative of if else
+// let result = age >= 18 ? "you are aligible" : "you are not aligible"
+// document.write(result)
+
+// if(age >= 18 && gender == "male"){
+//     document.write("You are Aligible")
+// }
+// else if(age >= 18 && gender == "female"){
+//     document.write("You are Aligible")
+// }
+// else{
+//     document.write("You are not Aligible")
+// }
+
