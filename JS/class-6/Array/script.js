a15d7f446736a1da29471f0268028802d8415d1f
Maaz Ahmad <maazahmad21988@gmail.com>
2026-08-30 11:05:37 +0500

    Class 6 | Array | Array Methods like push, pop,unshift,shift

---
+++ JS/class-6/Array/script.js
@@ -0,0 +1,14 @@
+let students = ["Ali", "Ahmad", "Waqas", "Qasim", "Saqib"];
+console.log(students);
+
+students.push("Aqib"); // Add element at the last index
+console.log(students);
+
+students.pop(); //Remove element from the last
+console.log(students);
+
+students.unshift("Aqeel"); // Add element at the first index
+console.log(students);
+
+students.shift(); // Remove element from the last
+console.log(students);
