db82bdfc5cef73a581a18b1575787662afa3b007
Maaz Ahmad <maazahmad21988@gmail.com>
2026-09-20 18:57:09 +0500

    Printing the marks of each subject and the total marks

---
+++ JS/class-10/loopsQuiz/studentMarks.js
@@ -0,0 +1,8 @@
+let studentMarks =[78,85,92,67,88,73];
+
+// now printing the marks of each subject and the total marks
+
+for(marks of studentMarks){
+    console.log(marks);
+}
+console.log(marks += marks);
