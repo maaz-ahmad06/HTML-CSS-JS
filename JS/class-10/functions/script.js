665f50385d8535a2c1139c2458f8867be411618b
Maaz Ahmad <maazahmad21988@gmail.com>
2026-09-20 10:57:49 +0500

    Class 10 | Function Practice

---
+++ JS/class-10/functions/script.js
@@ -0,0 +1,16 @@
+/* 
+Syntax:
+function functionName(parameters) {
+    body of the function
+}
+
+calling a function:
+functionName(arguments);
+
+*/
+
+function welcome(name) {
+    console.log(`Welcome ${name}`);
+}
+
+welcome("John");
