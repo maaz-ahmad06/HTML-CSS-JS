69c1d55b07740c29690d7f2f6fd7fb442b6abc22
Maaz Ahmad <maazahmad21988@gmail.com>
2026-09-20 19:13:57 +0500

    calculating the total price of all products

---
+++ JS/class-10/loopsQuiz/shoppingCart.js
@@ -0,0 +1,10 @@
+let prices = [500, 1200, 350, 800, 150];
+
+// calculating the total price of all products
+let totalPrice = 0;
+
+for (let price of prices) {
+  totalPrice += price;
+}
+
+console.log(totalPrice);
