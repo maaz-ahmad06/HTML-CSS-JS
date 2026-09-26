f0ae908e8a203d9c7ad790f462e18f4830251d41
Maaz Ahmad <maazahmad21988@gmail.com>
2026-09-26 11:04:19 +0500

    Class 11 | functions

---
+++ JS/class-11/functions/script.js
@@ -0,0 +1,16 @@
+function add(a, b){
+    console.log(`${a} + ${b} = ${a + b}`);
+}
+add(2, 4);
+
+function introduction(name, age, city){
+    console.log(`My name is ${name}`);
+    console.log(`I am ${age} years old`);
+    console.log(`I am from ${city}`);
+}
+introduction("maaz", 22, "Orakzai");
+
+function greet(name="Guist"){
+    console.log("Hello " + name);
+}
+
